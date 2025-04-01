34b2ebfb4859eb720a9b3b20e316313eb69dd711
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-04-01 13:45:57 -0300

    desenvolvi para frontend o layout e para o backend em cadatsro

--- FuncionarioProj/Models/FuncionarioView.cs
+++ FuncionarioProj/Models/FuncionarioView.cs
@@ -1,17 +1,23 @@
-﻿namespace FuncionarioProj.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FuncionarioProj.Models
 {
     public class FuncionarioView
     {
         public Guid Id { get; set; }
 
+        [Display(Name = "Nome do Funcionario")]
         public string? NomeFuncionario { get; set; }
 
+        [Display(Name = "Nome da Empresa")]
         public string? NomeEmpresa { get; set; }
 
         public string? Setor { get; set; }
 
+        [Display(Name = "Data de Inicio")]
         public DateTime DataInicio { get; set; }
 
+        [Display(Name = "Data de Termino")]
         public DateTime DataTermino { get; set; }
 
         public bool Ativo { get; set; }
